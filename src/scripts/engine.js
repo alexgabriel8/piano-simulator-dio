ce33a57076d943eec88b554b0afd1a82b9e66498
alexgabriel8 <alexgabriels8@outlook.com>
2023-12-16 06:26:01 -0300

    Game state equal to "Mapeando teclas permitidas"

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,21 +1,25 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let mappedKeys = [];
+
 const playTune = (key) => {
     const audio = new Audio(`src/tunes/${key}.wav`)
     audio.play()
 }
 
 pianoKeys.forEach(key => {
-    console.log(key.dataset.key)
     key.addEventListener("click", () => playTune(key.dataset.key));
+    mappedKeys.push(key.dataset.key)
 })
 
 document.addEventListener("keydown", (e) => {
-    playTune(e.key)
+    if(mappedKeys.includes(e.key)) {
+        playTune(e.key)
+        const clickedKey = document.querySelector(`[data-key="${e.key}"]`);
+        clickedKey.classList.add("active")
+        setTimeout(() => {
+            clickedKey.classList.remove("active")
+        }, 150);
+    }
 
-    const clickedKey = document.querySelector(`[data-key="${e.key}"]`);
-    clickedKey.classList.add("active")
-    setTimeout(() => {
-        clickedKey.classList.remove("active")
-    }, 150);
 })
